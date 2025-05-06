0200ce7cb4869e6ece44563bac7e10212aadae30
Mrudula Rani <mrudularani2000@gmail.com>
2025-05-05 23:14:09 -0500

    Merge remote-tracking branch 'origin/main' into main

--- backend/main.py
+++ backend/main.py
@@ -131,4 +131,4 @@ async def serve_home(request: Request):
 
 # Run FastAPI server
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8001)
